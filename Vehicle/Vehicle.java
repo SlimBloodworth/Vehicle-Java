e2ca4898f010c4df060ad41a3d46dd1bb1add3d2
Michelle Bloodworth <83024708+SlimBloodworth@users.noreply.github.com>
2021-09-27 12:40:05 -0500

    Add files via upload

---
+++ Vehicle/Vehicle.java
@@ -0,0 +1,18 @@
+public class Vehicle
+{
+	public static void main (String []args)
+	{
+		
+		System.out.println("                 ___________|)");
+		System.out.println("                ||           \\");
+		System.out.println("                ||            \\");
+		System.out.println("                ||              \\");
+		System.out.println(" _________________________________________");
+		System.out.println("(|=______________| --            |______=|)");
+		System.out.println(" |               |               |       |");
+		System.out.println(" |  ___          |               |  ___  |");
+		System.out.println(" |_/   \\_________|_______________|_/   \\_|");
+		System.out.println("  |     |                         |     |");
+		System.out.println("   \\___/                           \\___/");
+		}
+	}
